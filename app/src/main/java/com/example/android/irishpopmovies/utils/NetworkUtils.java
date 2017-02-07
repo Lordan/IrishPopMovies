3aad58115e670b0fd41ec1253199e909182bc751
Harald Schlindwein <harald.schlindwein@schneider-electric.com>
2017-02-07 14:29:56 +0000

    adding logging and gradle api key injection

--- app/src/main/java/com/example/android/irishpopmovies/utils/NetworkUtils.java
+++ app/src/main/java/com/example/android/irishpopmovies/utils/NetworkUtils.java
@@ -28,7 +28,7 @@ public class NetworkUtils {
             "http://image.tmdb.org/t/p";
 
     private final static String PARAM_API_KEY = "api_key";
-    private final static String TMDB_API_KEY = "PUT_API_KEY_IN_HERE";
+    private final static String TMDB_API_KEY = BuildConfig.API_KEY;
 
     public static boolean isOnline(Context context) {
         ConnectivityManager cm =
